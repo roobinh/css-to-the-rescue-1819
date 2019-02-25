60e7a171a82e347a79683195a99c844207df947b
roobinh <roobin1999@gmail.com>
2019-02-25 12:47:20 +0100

    working version

--- week 1/public/js/app.js
+++ week 1/public/js/app.js
@@ -1,2 +1,12 @@
 console.log("ik werk!");
 
+flexFont = function () {
+    var divs = document.getElementsByClassName("flexFont");
+    for(var i = 0; i < divs.length; i++) {
+        var relFontsize = divs[i].offsetWidth*0.05;
+        divs[i].style.fontSize = relFontsize+'px';
+    }
+};
+    
+window.onresize = function(event) {
+    flexFont();
