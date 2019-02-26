f6cbad4a9e10c9608f97e7c6cc93fc6c778226c4
roobinh <roobin1999@gmail.com>
2019-02-26 09:31:08 +0100

    cleaned

--- week 1/public/js/app.js
+++ week 1/public/js/app.js
@@ -1,12 +1,7 @@
 console.log("ik werk!");
 
-flexFont = function () {
-    var divs = document.getElementsByClassName("flexFont");
-    for(var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetWidth*0.05;
-        divs[i].style.fontSize = relFontsize+'px';
-    }
-};
-    
-window.onresize = function(event) {
-    flexFont();
+function clicked() {
+    console.log("frontpage clicked!")
+    // document.getElementById("frontpage").style.display = none;
+    // document.getElementById("homepage").style.display = flex;
+}
