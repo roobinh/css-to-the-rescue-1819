3d237b773cd355dc769fcda0ea23826c42f01cee
roobinh <roobin1999@gmail.com>
2019-02-26 12:30:05 +0100

    code cleanup

--- week 1/public/js/app.js
+++ week 1/public/js/app.js
@@ -1,17 +1,15 @@
-console.log("ik werk!");
-
+//When frontpage is clicked, directing to homepage
 function clicked() {
     console.log("frontpage clicked!")
     document.querySelector("[data-front]").style.display = 'none';
     document.querySelector("[data-home]").style.display = 'flex';
 }
 
+//When scrolling to drinks, drinks fly in
 $(window).scroll(function(){
     var a = document.scrollingElement.scrollTop;
 
     if(a > 200) {
         document.querySelector("[data-flying]").style.display = 'block';
     }
-
-    console.log("scroll value = " + a);
 });
