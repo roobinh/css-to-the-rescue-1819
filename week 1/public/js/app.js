604bd9defa653194d9700b77b4e1d7dd6ae396b6
roobinh <roobin1999@gmail.com>
2019-02-26 11:38:13 +0100

    huge update

--- week 1/public/js/app.js
+++ week 1/public/js/app.js
@@ -5,3 +5,13 @@ function clicked() {
     document.querySelector("[data-front]").style.display = 'none';
     document.querySelector("[data-home]").style.display = 'flex';
 }
+
+$(window).scroll(function(){
+    var a = document.scrollingElement.scrollTop;
+
+    if(a > 200) {
+        document.querySelector("[data-flying]").style.display = 'block';
+    }
+
+    console.log("scroll value = " + a);
+});
