2dab31f5cdc34ad3d53068a3a636da8eacd9753b
roobinh <roobin1999@gmail.com>
2019-02-27 15:47:17 +0100

    css added

--- week 1/public/js/app.js
+++ week 1/public/js/app.js
@@ -1,6 +1,5 @@
 //When frontpage is clicked, directing to homepage
 function clicked() {
-    console.log("frontpage clicked!")
     document.querySelector("[data-front]").style.display = 'none';
     document.querySelector("[data-home]").style.display = 'flex';
 }
